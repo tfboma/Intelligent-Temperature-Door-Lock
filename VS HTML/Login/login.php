e5a81240c8e1f59b59ce9d639ed85fa290ff2ed1
YingqiWang <52066681+tfboma@users.noreply.github.com>
2021-04-10 23:10:56 +0800

    Update login.php

--- VS HTML/Login/login.php
+++ VS HTML/Login/login.php
@@ -4,14 +4,18 @@ include('../connnect.php');
 mysqli_query($conn,"set names utf8");
 $Username = $_GET['Username'];
 $password = $_GET['password'];
-$check = mysqli_query($conn,"select * from User where Username='$Username' and password = '$password'");
-if($check)
+
+$check = mysql_query($conn,"select Username from User where Username='$Username' and password = '$password' limit 1");
+
+$result = mysql_frech_array($check);
+
+if($result)
 {
-    echo "success!";
+    echo 'success! Click to jump <a href ="file:///F:/VS%20HTML/Main/main.html">Main</a>';
 }
 else
 {
-    echo "failed!";
+    echo 'failed! Wrong user name or password! Click to return <a href = "javascript:history.back(-1);">Return</a>';
 }
 
 ?>
